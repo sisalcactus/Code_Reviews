4a3f9e581a6bf450eff9168c001c4974d3c46ce1
sisalcactus <hsiangchang2000@gmail.com>
2023-06-15 13:33:27 -0400

    More suggestions added.

--- Code Review for Ramiz (Assignment 2).R
+++ Code Review for Ramiz (Assignment 2).R
@@ -50,19 +50,23 @@ if(is.numeric(as.numeric(user_input)) &
   # 3. Lines 10-14: I appreciate the organization. Great work.
   # 4. Line 12: A minor correction to the comment "(2) if input is a natural number": We should have "...is an integer"
         # Rationale: the mod function checks for integers including negative ones, which means not just natural numbers
-  # 5. Line 19: I think the priority being checked for is whether the input is an integer; maybe revisit the wording
-  # 6. Line 23: Good work with using nchar(); this wasn't explicitly asked for so you went above and beyond with this one
-  # 7. Line 24:  It might be interesting to add a line of code to check for positive numbers (as.numeric(user_input)>0) 
-  # 8. Line 25: To avoid ambiguity, the wording in the comment can be tweaked to "each *digit* from the input" as the input itself itself is number
-  # 9. Line 26: I like the code for its elegance and simplicity. Well done, Ramiz. If you were interested, another way to find the three numbers are to use %% (to extract the rightmost digit), floor() (to extract the leftmost digit), and those two extracted digits to extract the middle digit.
+  # 5. Lines 17 and 20: These look correct; great work with the validity check design
+  # 6. Line 19: I think the priority being checked for is whether the input is an integer; maybe revisit the wording
+  # 7. Line 23: Good work with using nchar(); this wasn't explicitly asked for so you went above and beyond with this one
+  # 8. Line 24:  It might be interesting to add a line of code to check for positive numbers (as.numeric(user_input) > 0) 
+  # 9. Line 25: To avoid ambiguity, the wording in the comment can be tweaked to "each *digit* from the input" as the input itself itself is number
+  # 10. Line 26: I like the code for its elegance and simplicity. Well done, Ramiz. If you were interested, another way to find the three numbers are to use %% (to extract the rightmost digit), floor() (to extract the leftmost digit), and those two extracted digits to extract the middle digit.
       # Although a bit long, it can be a fun brainteaser (an unconventional approach)
           # third_digit <- value %% 10
           # first_digit <- floor(value/100)
           # second_digit <- (value - third_digit - first_digit*100)/10
-  # 10. Line 26: It seems that there is a bug with the condition in the if statement (none of the error messages you created appear) if we input "-11" or other negative two-digit numbers.
-      # To resolve this, you can add the test for positive numbers (please see my 7th comment);
-      # the reason this happens is because right now the nchar() function sees "-11" as a 3 digit number and the other validity checks don't catch it. In other words, numbers like -11 will all be deemed "valid," ultimately being subjected to calculation. This then yields NAs but does not prompt any error messages.
-  # 11. As a thought, I suggest capitalizing "armstrong" and "boolean" in the comments (just for accuracy/grammar)
+  # 11. Line 26: It seems that there is a bug with the condition in the if statement (none of the error messages you created appear) if we input a negative two-digit number (say, -10).
+      # The reason this happens is because right now none of the validity check seem to catch it: -10, being an integer, passes checks for (1) and (2) and the nchar() function sees it as a 3-digit  character.
+      # In other words, numbers like -10 will all be deemed "valid" according to your checks (1), (2), and (3), ultimately permitting the input to end up being calculated.
+      # This then yields NAs in the calculation, with no error messages (that you created) being printed.
+      # To resolve this bug, you can add the test for positive numbers (please see my 7th comment);
+  # 12. Lines 28 and 32: I believe the comments should be switched
+  # 13. As a thought, I suggest capitalizing "armstrong" and "boolean" in the comments (just for accuracy/grammar)
 
 
 
