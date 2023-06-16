6ff5864a675e21059bba355b5e0ca45509e9df5c
sisalcactus <hsiangchang2000@gmail.com>
2023-06-16 18:50:30 -0400

    Final edits (e.g., fixing typos).

--- Code Review for Ramiz (Assignment 2).R
+++ Code Review for Ramiz (Assignment 2).R
@@ -52,19 +52,19 @@ if(is.numeric(as.numeric(user_input)) &
         # Rationale: the mod function checks for integers including negative ones, which means not just natural numbers
   # 5. Lines 17 and 20: These look correct; great work with the validity check design
   # 6. Line 19: I think the priority being checked for is whether the input is an integer; maybe revisit the wording
-  # 7. Line 23: Good work with using nchar(); this wasn't explicitly asked for so you went above and beyond with this one
+  # 7. Line 23: Good work with using nchar() to check that the input has only 3 digits; this wasn't explicitly asked for so you went above and beyond with this one
   # 8. Line 24:  It might be interesting to add a line of code to check for positive numbers (as.numeric(user_input) > 0)
-  # 9. Line 25: To avoid ambiguity, the wording in the comment can be tweaked to "each *digit* from the input" as the input itself itself is number
+  # 9. Line 25: To avoid ambiguity, the wording in the comment can be tweaked to "each *digit* from the input" as the input itself itself is supposed to be a number
   # 10. Line 26: I like the code for its elegance and simplicity. Well done, Ramiz. If you were interested, another way to find the three numbers are to use %% (to extract the rightmost digit), floor() (to extract the leftmost digit), and those two extracted digits to extract the middle digit.
       # Rationale: Although a bit long, it can be a fun brainteaser (an unconventional approach)
           # third_digit <- user_input %% 10
           # first_digit <- floor(user_input/100)
           # second_digit <- (user_input - third_digit - first_digit*100)/10
   # 11. Line 26: It seems that there is a bug with the condition in the if statement (none of the error messages you created appear) if we input a negative two-digit number (say, -10).
-      # The reason this happens is because right now none of the validity check seem to catch it: -10 passes checks for (1) and (2) as it is an integer; it also passes the check for (3) as the nchar() function sees it as a 3-digit character.
+      # The reason this happens is because right now none of the validity checks you have seem to catch it: -10 passes checks for (1) and (2) as it is an integer; it also passes the check for (3) as the nchar() function sees it as a 3-digit character.
       # In other words, numbers like -10 will all be deemed "valid" according to your checks (1), (2), and (3), ultimately permitting the input to end up being calculated.
       # This then yields NAs in the calculation, with no error messages (that you created) being printed.
-      # To resolve this bug, you can add the test for positive numbers (please see my 7th comment);
+      # To resolve this bug, you can add the test for positive numbers (please see my 8th comment);
   # 12. Line 26: Another bug I found was that inputting 000 also prompts the message that I inputted a narcissistic number so maybe we could add a line of code to resolve this (another validity check)
   # 13. Lines 28 and 32: I believe the comments should be switched
   # 14. Lines 36 and onward: I appreciate the distinction between (1) being false (great application of is.na()) and (2)/(3) being false, prompting different error messages.
